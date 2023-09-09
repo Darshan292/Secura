c86aaf17d1a6b6438396d883d23e4aea15a45be1
Darshu20902 <darshanshetty20902@gmail.com>
2023-09-09 13:38:16 +0530

    Second Commit

--- EncryptDecrypt.py
+++ EncryptDecrypt.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 from dotenv import set_key
 import os
 import keyboard
+import subprocess
 
 load_dotenv()
+
 
 def generatekey():
     return Fernet.generate_key()
@@ -18,30 +20,37 @@ def encrypt(filepath,filename, fileextn, key):
     with open(filepath + "/" + filename + "." + fileextn, "wb") as file:
         file.write(encrypted_data)
 
-def decrypt(filepath,filename, fileextn, key):
-    cipher_suite = Fernet(key)
+def decrypt(filepath,filename, fileextn, vkey):
+    cipher_suite = Fernet(vkey.encode('utf-8'))
     with open(filepath + "/" + filename + "." + fileextn, "rb") as file:
         data=file.read()
         file.close
     decrypted_data=cipher_suite.decrypt(data)
     with open(filepath + "/" + filename + "." + fileextn, "wb") as file:
         file.write(decrypted_data)
-
         
 def encrdecr():
-    key=generatekey()
+    
     filepath = os.getenv('encrypted_files')
     file_items = os.listdir(filepath)
+    print("key for decreption: " + os.getenv("key"))
+    for item in file_items:
+        file_name, file_extn = item.split('.')
+        decrypt(filepath,file_name, file_extn, os.getenv("key"))
+        print("Decrypted")
+    subprocess.Popen(os.getenv('Encrypted_files_path')) 
+    while True:
+        if keyboard.is_pressed('e'):
+            break
 
+    key=generatekey()
+    set_key(".env","key",key.decode("utf-8"))
+    print(key)
     for item in file_items:
         file_name, file_extn = item.split('.')
         encrypt(filepath,file_name, file_extn, key)
-    while True:
-        if keyboard.is_pressed('d'):
-            break
-    for item in file_items:
-        file_name, file_extn = item.split('.')
-        decrypt(filepath,file_name, file_extn, key)
+        print('Encrypted')
+    
 
 
 
